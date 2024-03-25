38423680c3e073e70968b8f819d65d6dd29791c9
iagop <iagopuzer06@hotmail.com>
2024-03-25 17:16:55 -0300

    criando o botão de deletar articles

--- src/app/Articles/page.js
+++ src/app/Articles/page.js
@@ -2,8 +2,9 @@
 import { useState, useEffect } from "react";
 import ArticleCard from "../Components/articleCard";
 
-export default function Home() {
+export default function ArticlePage() {
   const [articles, setArticles] = useState([]);
+
   const fetchAllArticles = async () => {
     try {
       const response = await fetch("../api/articles");
@@ -13,13 +14,24 @@ export default function Home() {
       console.error(error);
     }
   };
+
+  const handleDelete = (articleId) => {
+    setArticles(articles.filter((article) => article.id !== articleId));
+    fetchAllArticles();
+  };
+
   useEffect(() => {
     fetchAllArticles();
   }, []);
+
   return (
     <div className="w-1/2 mx-auto flex justify-between">
-      {articles.map((article, index) => (
-        <ArticleCard key={article.id} article={article} />
+      {articles.map((article) => (
+        <ArticleCard
+          key={article.id}
+          article={article}
+          onDelete={handleDelete}
+        />
       ))}
     </div>
   );
--- src/app/Components/articleCard.js
+++ src/app/Components/articleCard.js
@@ -1,7 +1,21 @@
-export default function ArticleCard({ article }) {
+export default function ArticleCard({ article, onDelete }) {
+  const handleDelete = async () => {
+    try {
+      const response = await fetch(`../api/articles/${article.id}`, {
+        method: "DELETE",
+      });
+      if (response.ok) {
+        onDelete(article.id);
+      } else {
+        console.error("Failed to delete article");
+      }
+    } catch (error) {
+      console.error("Error deleting article:", error);
+    }
+  };
+
   return (
     <>
-      {/*<!-- Component: Basic card --> */}
       <div className="overflow-hidden rounded bg-white text-slate-500 shadow-md shadow-slate-200">
         <div className="p-6">
           <div className="flex flex-col">
@@ -10,10 +24,15 @@ export default function ArticleCard({ article }) {
             </h3>
             <h6>{article.description}</h6>
           </div>
-          <p>{article.body}.</p>
+          <p>{article.body}</p>
+          <button
+            className="bg-red-200 p-2 rounded-md text-black"
+            onClick={handleDelete}
+          >
+            Delete
+          </button>
         </div>
       </div>
-      {/*<!-- End Basic card --> */}
     </>
   );
 }
