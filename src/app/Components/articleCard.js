4b9a41fe820bf0348ee4bd33acc2112a66d93962
iagop <iagopuzer06@hotmail.com>
2024-03-27 16:44:51 -0300

    trocando o conteudo da pagina principal

--- src/app/Components/articleCard.js
+++ src/app/Components/articleCard.js
@@ -1,4 +1,15 @@
+"use client";
+import { useState } from "react";
+import EditedCardModal from "./Modals/EditedCardModal";
+
 export default function ArticleCard({ article, onDelete }) {
+  const [showModal, setShowModal] = useState(false);
+  const [editedArticle, setEditedArticle] = useState({
+    title: article.title,
+    description: article.description,
+    body: article.body,
+  });
+
   const handleDelete = async () => {
     try {
       const response = await fetch(`../api/articles/${article.id}`, {
@@ -14,6 +25,34 @@ export default function ArticleCard({ article, onDelete }) {
     }
   };
 
+  const handleEdit = () => {
+    setShowModal(true);
+  };
+
+  const handleSaveEdit = async (updatedArticle) => {
+    try {
+      const response = await fetch(`../api/articles/${article.id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(updatedArticle),
+      });
+      if (response.ok) {
+        setEditedArticle(updatedArticle);
+      } else {
+        console.error("Failed to update article");
+      }
+    } catch (error) {
+      console.error("Error updating article:", error);
+    }
+    setShowModal(false);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
   return (
     <>
       <div className="overflow-hidden rounded bg-white text-slate-500 shadow-md shadow-slate-200">
@@ -26,13 +65,26 @@ export default function ArticleCard({ article, onDelete }) {
           </div>
           <p>{article.body}</p>
           <button
-            className="bg-red-200 p-2 rounded-md text-black"
+            className="bg-red-200 p-2 rounded-md text-black mr-2"
             onClick={handleDelete}
           >
             Delete
           </button>
+          <button
+            className="bg-red-200 p-2 rounded-md text-black"
+            onClick={handleEdit}
+          >
+            Editar
+          </button>
         </div>
       </div>
+      <EditedCardModal
+        isOpen={showModal}
+        onClose={handleCloseModal}
+        onSave={handleSaveEdit}
+        editedArticle={editedArticle}
+        setEditedArticle={setEditedArticle}
+      />
     </>
   );
 }
