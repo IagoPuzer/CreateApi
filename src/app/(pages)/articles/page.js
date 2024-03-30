119a79fc790e9767110a31c907a4fa522bb4be4e
iagop <iagopuzer06@hotmail.com>
2024-03-30 17:47:39 -0300

    Ajuste na page de artigos

--- src/app/(pages)/articles/page.js
+++ src/app/(pages)/articles/page.js
@@ -3,15 +3,12 @@ import { useState, useEffect } from "react";
 import ArticleCard from "../../Components/articleCard";
 import { useRouter } from "next/navigation";
 import { getSession } from "next-auth/react";
+import LogoutButton from "../../Components/buttons/LogoutButton";
+import CreateArticleForm from "@/app/Components/forms/CreateArticleForm";
 
 export default function Home() {
   const [articles, setArticles] = useState([]);
   const [showForm, setShowForm] = useState(false);
-  const [newArticleData, setNewArticleData] = useState({
-    title: "",
-    description: "",
-    body: "",
-  });
 
   const fetchAllArticles = async () => {
     try {
@@ -23,7 +20,7 @@ export default function Home() {
     }
   };
 
-  const createNewArticle = async () => {
+  const createNewArticle = async (newArticleData) => {
     try {
       const response = await fetch("../../api/articles", {
         method: "POST",
@@ -36,7 +33,6 @@ export default function Home() {
         const newArticle = await response.json();
         setArticles([...articles, newArticle]);
         setShowForm(false);
-        setNewArticleData({ title: "", description: "", body: "" });
       } else {
         console.error("Failed to create article");
       }
@@ -77,60 +73,19 @@ export default function Home() {
     <div className="container mx-auto px-4 py-8">
       <div className="flex justify-between items-center mb-8">
         <h1 className="text-3xl font-bold">Articles</h1>
-        <button
-          onClick={() => setShowForm(true)}
-          className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded"
-        >
-          Criar artigo
-        </button>
+        <div className="flex gap-6">
+          <button
+            onClick={() => setShowForm(true)}
+            className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded"
+          >
+            Criar artigo
+          </button>
+          <LogoutButton />
+        </div>
       </div>
 
       {showForm && (
-        <div className="bg-gray-100 p-4 rounded mb-8">
-          <input
-            type="text"
-            placeholder="Title"
-            className="block w-full border border-gray-300 rounded mb-2 p-2 text-black"
-            value={newArticleData.title}
-            onChange={(e) =>
-              setNewArticleData({ ...newArticleData, title: e.target.value })
-            }
-          />
-          <input
-            type="text"
-            placeholder="Description"
-            className="text-black block w-full border border-gray-300 rounded mb-2 p-2"
-            value={newArticleData.description}
-            onChange={(e) =>
-              setNewArticleData({
-                ...newArticleData,
-                description: e.target.value,
-              })
-            }
-          />
-          <textarea
-            placeholder="Body"
-            className="text-black block w-full border border-gray-300 rounded mb-2 p-2"
-            value={newArticleData.body}
-            onChange={(e) =>
-              setNewArticleData({ ...newArticleData, body: e.target.value })
-            }
-          />
-          <div className="flex gap-4">
-            <button
-              onClick={createNewArticle}
-              className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded"
-            >
-              Criar
-            </button>
-            <button
-              onClick={closeForm}
-              className="bg-gray-500 hover:bg-gray-600 text-white font-semibold py-2 px-4 rounded"
-            >
-              fechar
-            </button>
-          </div>
-        </div>
+        <CreateArticleForm onSubmit={createNewArticle} onClose={closeForm} />
       )}
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
