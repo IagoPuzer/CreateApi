dfedb83102389b3dcbf24b84ab77f6473a424fe8
iagop <iagopuzer06@hotmail.com>
2024-04-01 14:24:34 -0300

    Fazendo com que a verificação do login deixe de ser local e se torne uma busca no BD para saber se o usuário existe

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,8 @@
 import NextAuth from "next-auth";
+import { PrismaClient } from "@prisma/client";
 import CredentialsProvider from "next-auth/providers/credentials";
+
+const prisma = new PrismaClient();
 
 const handler = NextAuth({
   pages: {
@@ -9,24 +12,23 @@ const handler = NextAuth({
     CredentialsProvider({
       name: "Credentials",
       credentials: {
-        email: { label: "Email", type: "email" },
+        email: { label: "Email", type: "text" },
         password: { label: "Password", type: "password" },
       },
       async authorize(credentials) {
-        if (!credentials) {
+        const user = await prisma.user.findUnique({
+          where: { email: credentials.email },
+        });
+
+        if (!user || user.password !== credentials.password) {
           return null;
         }
-        if (
-          credentials.email === "iagopuzer06@hotmail.com" &&
-          credentials.password === "123456"
-        ) {
-          return {
-            id: "1",
-            name: "Iago",
-            email: "iagopuzer06@hotmail.com",
-          };
-        }
-        return null;
+        return {
+          id: user.id,
+          email: user.email,
+          name: user.name,
+          email: user.email,
+        };
       },
     }),
   ],
